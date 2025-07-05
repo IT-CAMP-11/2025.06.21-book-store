542f8e65cffe3518a1537148afd51c8eea0e9207
Mateusz <mat.bereda@gmail.com>
2025-07-05 09:40:52 +0200

    update

--- src/main/resources/static/orders.js
+++ src/main/resources/static/orders.js
@@ -2,16 +2,16 @@ function showDetails(event) {
     // Zatrzymujemy propagację (opcjonalnie, jeśli masz inne kliknięcia)
     event.stopPropagation();
 
-    // Znajdujemy kliknięty <tr>
+    // Znajdujemy kliknięty <div>>
     const clickedRow = event.currentTarget;
 
     // Szukamy następnego wiersza .details
-    const detailsRow = clickedRow.nextElementSibling;
+    const detailsRow = clickedRow.querySelector(".details");
 
     // Jeśli znaleziony i ma klasę details
     if (detailsRow && detailsRow.classList.contains('details')) {
       if (detailsRow.style.display === 'none' || detailsRow.style.display === '') {
-        detailsRow.style.display = 'table-row';
+        detailsRow.style.display = 'block';
       } else {
         detailsRow.style.display = 'none';
       }
